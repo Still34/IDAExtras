d0823be0df7c6822549ea28ef2cca2a3b2427516
Still Hsu <dev@stillu.cc>
2025-09-15 16:54:40 +0800

    Lint per pep8 default
    Signed-off-by: Still Hsu <dev@stillu.cc>

--- IDAExtras.py
+++ IDAExtras.py
@@ -1,6 +1,7 @@
-## Author: @xorhex
-## Copyright: 2024
+# Author: @xorhex
+# Copyright: 2024
 
+import struct
 __author__ = "https://infosec.exchange/@xorhex"
 
 import idaapi
@@ -13,130 +14,137 @@ from idaextras.IDAExtrasListExportsForm import ExportListUI
 
 ida_ver = get_ida_version()
 if ida_ver >= 9.2:
-  from PySide6 import QtCore
-  from PySide6 import QtGui
-  from PySide6 import QtWidgets
-  bwn_hex = idaapi.BWN_HEXVIEW
+    from PySide6 import QtCore
+    from PySide6 import QtGui
+    from PySide6 import QtWidgets
+    bwn_hex = idaapi.BWN_HEXVIEW
 else:
-  from PyQt5 import QtCore
-  from PyQt5 import QtGui
-  from PyQt5 import QtWidgets
-  bwn_hex = idaapi.BWN_DUMP
+    from PyQt5 import QtCore
+    from PyQt5 import QtGui
+    from PyQt5 import QtWidgets
+    bwn_hex = idaapi.BWN_DUMP
 
-import struct
 
 ACTION_NAME_IDA_EXTRAS = "idaextras"
 
 logger = Logger()
 
 
-## Custom Export Window
+# Custom Export Window
 
 CUSTOM_EXPORT_WINDOW_DISPLAY_NAME = f"exports"
 CUSTOM_EXPORT_WINDOW = f"{ACTION_NAME_IDA_EXTRAS}:{CUSTOM_EXPORT_WINDOW_DISPLAY_NAME}"
 
+
 class ExportsDisplay(idaapi.action_handler_t):
-  def __init__(self):
-    idaapi.action_handler_t.__init__(self)
-    self.exports = ExportListUI()
+    def __init__(self):
+        idaapi.action_handler_t.__init__(self)
+        self.exports = ExportListUI()
 
-  def activate(self, ctx):
-      self.exports.Show(f"IDA Extras: {CUSTOM_EXPORT_WINDOW_DISPLAY_NAME.title()}")
+    def activate(self, ctx):
+        self.exports.Show(f"IDA Extras: {CUSTOM_EXPORT_WINDOW_DISPLAY_NAME.title()}")
 
-  def update(self, ctx):
-    return idaapi.AST_ENABLE_ALWAYS
+    def update(self, ctx):
+        return idaapi.AST_ENABLE_ALWAYS
+
 
 if idaapi.register_action(idaapi.action_desc_t(
-  CUSTOM_EXPORT_WINDOW,
-  CUSTOM_EXPORT_WINDOW_DISPLAY_NAME.title(),
-  ExportsDisplay(),
-  "Ctrl-Alt-E")):
-  idaapi.attach_action_to_menu("View/IDA Extras Views/", CUSTOM_EXPORT_WINDOW, idaapi.SETMENU_INS)
-  logger.log(CUSTOM_EXPORT_WINDOW, "register_action", "Attached")
+        CUSTOM_EXPORT_WINDOW,
+        CUSTOM_EXPORT_WINDOW_DISPLAY_NAME.title(),
+        ExportsDisplay(),
+        "Ctrl-Alt-E")):
+    idaapi.attach_action_to_menu("View/IDA Extras Views/", CUSTOM_EXPORT_WINDOW, idaapi.SETMENU_INS)
+    logger.log(CUSTOM_EXPORT_WINDOW, "register_action", "Attached")
 else:
-  idaapi.unregister_action(CUSTOM_EXPORT_WINDOW)
+    idaapi.unregister_action(CUSTOM_EXPORT_WINDOW)
 
 
 class context_handler_copy_bytes(idaapi.action_handler_t):
-  def __init__(self):
-    super().__init__()
+    def __init__(self):
+        super().__init__()
 
-  def activate(self, ctx):
-    ea = idc.read_selection_start()
-    bites = []
-    
-    instr_bites = idaapi.get_bytes(ea, idc.read_selection_end() - idc.read_selection_start())
-    for b in instr_bites:
-      bites.append(f'{b:02x}')
+    def activate(self, ctx):
+        ea = idc.read_selection_start()
+        bites = []
 
-    cb = QtWidgets.QApplication.instance().clipboard()
-    cb.clear(mode = cb.Clipboard)
-    cb.setText(f'{" ".join(bites)}', mode=cb.Clipboard)
+        instr_bites = idaapi.get_bytes(ea, idc.read_selection_end() - idc.read_selection_start())
+        for b in instr_bites:
+            bites.append(f'{b:02x}')
 
-  def update(self, ctx):
-    return super().update(ctx)
+        cb = QtWidgets.QApplication.instance().clipboard()
+        cb.clear(mode=cb.Clipboard)
+        cb.setText(f'{" ".join(bites)}', mode=cb.Clipboard)
 
-### Region
-#### Helper Functions
-#### Convert hard coded data into sockaddr_in.sin_port and sockaddr_in.sin_addr types
+    def update(self, ctx):
+        return super().update(ctx)
+
+# Region
+# Helper Functions
+# Convert hard coded data into sockaddr_in.sin_port and sockaddr_in.sin_addr types
+
+
 def dword_to_ip(dword: int) -> str:
-  ip = []
-  for x in range(4):
-    ip.append(str((dword >> (8 * x)) & 0xff))
-  return '.'.join(ip)
+    ip = []
+    for x in range(4):
+        ip.append(str((dword >> (8 * x)) & 0xff))
+    return '.'.join(ip)
+
 
 def is_valid_ip(ip: str) -> bool:
-  """
-  This function is not perfect as an IP address can end in 255 but the likelyhood that a
-  threat actor is using it is low as it's reserved.  Same with IP addresses that start
-  with 0.
-  """
-  parts = ip.split('.')
-  if len(parts) != 4:
-    return False
-  if int(parts[0], 10) == 0x0:
-    return False
-  if int(parts[3], 10) == 0xff:
-    return False
-  return True
+    """
+    This function is not perfect as an IP address can end in 255 but the likelyhood that a
+    threat actor is using it is low as it's reserved.  Same with IP addresses that start
+    with 0.
+    """
+    parts = ip.split('.')
+    if len(parts) != 4:
+        return False
+    if int(parts[0], 10) == 0x0:
+        return False
+    if int(parts[3], 10) == 0xff:
+        return False
+    return True
+
 
 def word_to_port(word: int) -> str:
-  return str(struct.unpack('>H', struct.pack('<H', word))[0])
-### End Region
+    return str(struct.unpack('>H', struct.pack('<H', word))[0])
+# End Region
+
 
 class context_handler_set_cmt(idaapi.action_handler_t):
-  def __init__(self, value):
-    super().__init__()
-    self.value= value
+    def __init__(self, value):
+        super().__init__()
+        self.value = value
 
-  def activate(self, ctx):
-    idaapi.set_cmt(idaapi.get_screen_ea(), self.value, False)
+    def activate(self, ctx):
+        idaapi.set_cmt(idaapi.get_screen_ea(), self.value, False)
 
-  def update(self, ctx):
-    return super().update(ctx)
+    def update(self, ctx):
+        return super().update(ctx)
+
 
 class ContextHooks(idaapi.UI_Hooks):
-  def finish_populating_widget_popup(self, form, popup) -> None:
-    flags = idaapi.get_flags(idaapi.get_screen_ea())
-    if idaapi.get_widget_type(form) in [idaapi.BWN_DISASM, bwn_hex]:
-      if idc.read_selection_start() != idaapi.BADADDR and idc.read_selection_end() != idaapi.BADADDR:
-        ## This is a quick simple context menu item to Copy Bytes (native functionality is Shift+E)
-        action_copy_bytes = idaapi.action_desc_t(None, f"Copy Bytes", context_handler_copy_bytes())
-        idaapi.attach_dynamic_action_to_popup(form, popup, action_copy_bytes, None, idaapi.SETMENU_FIRST)
-    if idaapi.get_widget_type(form) in [idaapi.BWN_DISASM]:
-      if idaapi.is_code(flags):
-        if not idaapi.is_defarg(flags, idaapi.get_opnum()) or idaapi.is_numop(flags, idaapi.get_opnum()):
-          instr = idautils.DecodeInstruction(idaapi.get_screen_ea())
-          op = instr.ops[idaapi.get_opnum()]
-          if op.type == idaapi.o_imm:
-            if op.dtype == idaapi.dt_dword:
-              ip = dword_to_ip(op.value & 0xffffffff)
-              if is_valid_ip(ip):
-                action_dword_to_ipv4 = idaapi.action_desc_t(None, f"sockaddr_in.sin_addr: {ip}", context_handler_set_cmt(ip))
-                idaapi.attach_dynamic_action_to_popup(form, popup, action_dword_to_ipv4, "Manual", idaapi.SETMENU_INS)
-            if op.dtype == idaapi.dt_word or (op.dtype == idaapi.dt_dword and op.value < 0x10000):
-              action_word_to_port = idaapi.action_desc_t(None, f"sockaddr_in.sin_port: {word_to_port(op.value & 0xffff)}", context_handler_set_cmt(word_to_port(op.value & 0xffff)))
-              idaapi.attach_dynamic_action_to_popup(form, popup, action_word_to_port, "Manual", idaapi.SETMENU_INS)
+    def finish_populating_widget_popup(self, form, popup) -> None:
+        flags = idaapi.get_flags(idaapi.get_screen_ea())
+        if idaapi.get_widget_type(form) in [idaapi.BWN_DISASM, bwn_hex]:
+            if idc.read_selection_start() != idaapi.BADADDR and idc.read_selection_end() != idaapi.BADADDR:
+                # This is a quick simple context menu item to Copy Bytes (native functionality is Shift+E)
+                action_copy_bytes = idaapi.action_desc_t(None, f"Copy Bytes", context_handler_copy_bytes())
+                idaapi.attach_dynamic_action_to_popup(form, popup, action_copy_bytes, None, idaapi.SETMENU_FIRST)
+        if idaapi.get_widget_type(form) in [idaapi.BWN_DISASM]:
+            if idaapi.is_code(flags):
+                if not idaapi.is_defarg(flags, idaapi.get_opnum()) or idaapi.is_numop(flags, idaapi.get_opnum()):
+                    instr = idautils.DecodeInstruction(idaapi.get_screen_ea())
+                    op = instr.ops[idaapi.get_opnum()]
+                    if op.type == idaapi.o_imm:
+                        if op.dtype == idaapi.dt_dword:
+                            ip = dword_to_ip(op.value & 0xffffffff)
+                            if is_valid_ip(ip):
+                                action_dword_to_ipv4 = idaapi.action_desc_t(None, f"sockaddr_in.sin_addr: {ip}", context_handler_set_cmt(ip))
+                                idaapi.attach_dynamic_action_to_popup(form, popup, action_dword_to_ipv4, "Manual", idaapi.SETMENU_INS)
+                        if op.dtype == idaapi.dt_word or (op.dtype == idaapi.dt_dword and op.value < 0x10000):
+                            action_word_to_port = idaapi.action_desc_t(None, f"sockaddr_in.sin_port: {word_to_port(op.value & 0xffff)}", context_handler_set_cmt(word_to_port(op.value & 0xffff)))
+                            idaapi.attach_dynamic_action_to_popup(form, popup, action_word_to_port, "Manual", idaapi.SETMENU_INS)
 
 
 hooks = ContextHooks()
--- idaextras/IDAExtrasListExportsForm.py
+++ idaextras/IDAExtrasListExportsForm.py
@@ -7,161 +7,160 @@ from idaextras.Helpers import get_ida_version
 
 ida_ver = get_ida_version()
 if ida_ver >= 9.2:
-  from PySide6 import QtCore
-  from PySide6 import QtGui
-  from PySide6 import QtWidgets
-  from PySide6.QtGui import QShortcut
+    from PySide6 import QtCore
+    from PySide6 import QtGui
+    from PySide6 import QtWidgets
+    from PySide6.QtGui import QShortcut
 else:
-  from PyQt5 import QtCore
-  from PyQt5 import QtGui
-  from PyQt5 import QtWidgets
-  from PyQt5.QtWidgets import QShortcut
+    from PyQt5 import QtCore
+    from PyQt5 import QtGui
+    from PyQt5 import QtWidgets
+    from PyQt5.QtWidgets import QShortcut
+
 
 class FunctionWalker():
-  def countCallInstructions(self, ea):
-    count = 0
-    func = idaapi.get_func(ea)
-    flowchart = idaapi.FlowChart(func)
+    def countCallInstructions(self, ea):
+        count = 0
+        func = idaapi.get_func(ea)
+        flowchart = idaapi.FlowChart(func)
 
-    for bb in flowchart:
-      ea = bb.start_ea
-      while ea < bb.end_ea:
-        instr = idautils.DecodeInstruction(ea)
-        if instr.itype in [idaapi.NN_call, idaapi.NN_callfi, idaapi.NN_callni]:
-          count +=1
-        ea = idaapi.next_head(instr.ea, bb.end_ea)
-    return count
+        for bb in flowchart:
+            ea = bb.start_ea
+            while ea < bb.end_ea:
+                instr = idautils.DecodeInstruction(ea)
+                if instr.itype in [idaapi.NN_call, idaapi.NN_callfi, idaapi.NN_callni]:
+                    count += 1
+                ea = idaapi.next_head(instr.ea, bb.end_ea)
+        return count
 
 
 class ExportTableModel(QtCore.QAbstractTableModel):
-  def __init__(self, data):
-    super(ExportTableModel, self).__init__()
-    self._data = data
-    self.columns = ["Name", "Address", "Ordinal", "Is Code", "Starting Mnem", "Call Count", "Basic Blocks Count", "Function Size"]
+    def __init__(self, data):
+        super(ExportTableModel, self).__init__()
+        self._data = data
+        self.columns = ["Name", "Address", "Ordinal", "Is Code", "Starting Mnem", "Call Count", "Basic Blocks Count", "Function Size"]
 
-  def data(self, index, role):
-    if role == QtCore.Qt.DisplayRole:
-      return self._data[index.row()][index.column()]
+    def data(self, index, role):
+        if role == QtCore.Qt.DisplayRole:
+            return self._data[index.row()][index.column()]
 
-  def headerData(self, section, orientation, role):
-    if role == QtCore.Qt.DisplayRole:
-      if orientation == QtCore.Qt.Horizontal:
-          return self.columns[section]
+    def headerData(self, section, orientation, role):
+        if role == QtCore.Qt.DisplayRole:
+            if orientation == QtCore.Qt.Horizontal:
+                return self.columns[section]
 
-  def rowCount(self, index):
-    return len(self._data)
+    def rowCount(self, index):
+        return len(self._data)
 
-  def columnCount(self, index):
-    return len(self._data[0])
+    def columnCount(self, index):
+        return len(self._data[0])
 
 
 class ExportListUI(idaapi.PluginForm):
-  def GetExportData(self):
-    fw = FunctionWalker()
-    data = []
-    for _, ordinal, ea, name in idautils.Entries():
-      iscode = idaapi.is_code(idaapi.get_flags(ea))
-      if iscode:
-        func = idaapi.get_func(ea)
-        flowchart = idaapi.FlowChart(func)
-        func_size = func.size()
-        bb_count = len(list(flowchart))
-        call_count = fw.countCallInstructions(ea)
-      else:
-        func_size = 'NA'
-        bb_count = 'NA'
-        call_count = 'NA'
+    def GetExportData(self):
+        fw = FunctionWalker()
+        data = []
+        for _, ordinal, ea, name in idautils.Entries():
+            iscode = idaapi.is_code(idaapi.get_flags(ea))
+            if iscode:
+                func = idaapi.get_func(ea)
+                flowchart = idaapi.FlowChart(func)
+                func_size = func.size()
+                bb_count = len(list(flowchart))
+                call_count = fw.countCallInstructions(ea)
+            else:
+                func_size = 'NA'
+                bb_count = 'NA'
+                call_count = 'NA'
 
-      if ordinal == ea:
-        ordi = "[main entry]"
-      else:
-        ordi = ordinal
+            if ordinal == ea:
+                ordi = "[main entry]"
+            else:
+                ordi = ordinal
 
-      data.append([name, hex(ea), ordi, str(iscode), idaapi.print_insn_mnem(ea), call_count, bb_count, func_size])
-    return data
+            data.append([name, hex(ea), ordi, str(iscode), idaapi.print_insn_mnem(ea), call_count, bb_count, func_size])
+        return data
 
-  def ActionCellDoubleClicked(self, item):
-    idx = self.model.columns.index("Address")
-    ea_item = item.siblingAtColumn(idx)
-    ea = self.proxymodel.data(ea_item)
-    idaapi.jumpto(int(ea,16))
+    def ActionCellDoubleClicked(self, item):
+        idx = self.model.columns.index("Address")
+        ea_item = item.siblingAtColumn(idx)
+        ea = self.proxymodel.data(ea_item)
+        idaapi.jumpto(int(ea, 16))
 
-  def ActionFilterClosedClicked(self, checked):
-    self.filterWidget.setVisible(False)
+    def ActionFilterClosedClicked(self, checked):
+        self.filterWidget.setVisible(False)
 
-  def ActionAutoFilterToggled(self, checked):
-    if checked:
-      self.isCodeFilterModel.setFilterRegularExpression(QtCore.QRegularExpression("[^False]", QtCore.QRegularExpression.CaseInsensitiveOption))
-      self.noRetnFilterModel.setFilterRegularExpression(QtCore.QRegularExpression("[^retn]", QtCore.QRegularExpression.CaseInsensitiveOption))
-    else:
-      self.isCodeFilterModel.setFilterRegularExpression(QtCore.QRegularExpression(".*", QtCore.QRegularExpression.CaseInsensitiveOption))
-      self.noRetnFilterModel.setFilterRegularExpression(QtCore.QRegularExpression(".*", QtCore.QRegularExpression.CaseInsensitiveOption))
+    def ActionAutoFilterToggled(self, checked):
+        if checked:
+            self.isCodeFilterModel.setFilterRegularExpression(QtCore.QRegularExpression("[^False]", QtCore.QRegularExpression.CaseInsensitiveOption))
+            self.noRetnFilterModel.setFilterRegularExpression(QtCore.QRegularExpression("[^retn]", QtCore.QRegularExpression.CaseInsensitiveOption))
+        else:
+            self.isCodeFilterModel.setFilterRegularExpression(QtCore.QRegularExpression(".*", QtCore.QRegularExpression.CaseInsensitiveOption))
+            self.noRetnFilterModel.setFilterRegularExpression(QtCore.QRegularExpression(".*", QtCore.QRegularExpression.CaseInsensitiveOption))
 
+    def ActionShowFilter(self):
+        self.filterWidget.setVisible(True)
+        self.filterText.setFocus()
 
-  def ActionShowFilter(self):
-    self.filterWidget.setVisible(True)
-    self.filterText.setFocus()
+    def ActionTextFilter(self, text):
+        self.proxymodel.setFilterRegularExpression(QtCore.QRegularExpression(text, QtCore.QRegularExpression.CaseInsensitiveOption.value | QtCore.QRegularExpression.MultilineOption.value))
 
-  def ActionTextFilter(self, text):
-    self.proxymodel.setFilterRegularExpression(QtCore.QRegularExpression(text, QtCore.QRegularExpression.CaseInsensitiveOption.value | QtCore.QRegularExpression.MultilineOption.value))
+    def OnCreate(self, form):
+        self.parent = self.FormToPyQtWidget(form)
+        self.layout = QtWidgets.QVBoxLayout()
 
-  def OnCreate(self, form):
-    self.parent = self.FormToPyQtWidget(form)
-    self.layout = QtWidgets.QVBoxLayout()
+        self.filterWidget = QtWidgets.QWidget()
+        self.filterWidget.setVisible(False)
+        self.filterlayout = QtWidgets.QHBoxLayout(self.filterWidget)
+        self.filterText = QtWidgets.QLineEdit()
+        self.filterText.textChanged.connect(self.ActionTextFilter)
+        self.closeButton = QtWidgets.QPushButton("X")
+        self.closeButton.setStyleSheet("color:rgb(93, 173, 226);font-weight: 900;")
+        self.closeButton.setFixedSize(QtCore.QSize(25, 25))
+        self.closeButton.clicked.connect(self.ActionFilterClosedClicked)
+        self.closeButton.setToolTip("Close filter")
+        self.autoFilterCheckbox = QtWidgets.QCheckBox("Auto Filter")
+        self.autoFilterCheckbox.setToolTip("Removes:\n- `retn` from `Starting Mnem`\n- `False` from `Is Code`")
+        self.autoFilterCheckbox.toggled.connect(self.ActionAutoFilterToggled)
+        self.filterlayout.addWidget(self.closeButton)
+        self.filterlayout.addWidget(self.autoFilterCheckbox)
+        self.filterlayout.addWidget(self.filterText)
 
-    self.filterWidget = QtWidgets.QWidget()
-    self.filterWidget.setVisible(False)
-    self.filterlayout = QtWidgets.QHBoxLayout(self.filterWidget)
-    self.filterText = QtWidgets.QLineEdit()
-    self.filterText.textChanged.connect(self.ActionTextFilter)
-    self.closeButton = QtWidgets.QPushButton("X")
-    self.closeButton.setStyleSheet("color:rgb(93, 173, 226);font-weight: 900;")
-    self.closeButton.setFixedSize(QtCore.QSize(25, 25))
-    self.closeButton.clicked.connect(self.ActionFilterClosedClicked)
-    self.closeButton.setToolTip("Close filter")
-    self.autoFilterCheckbox = QtWidgets.QCheckBox("Auto Filter")
-    self.autoFilterCheckbox.setToolTip("Removes:\n- `retn` from `Starting Mnem`\n- `False` from `Is Code`")
-    self.autoFilterCheckbox.toggled.connect(self.ActionAutoFilterToggled)
-    self.filterlayout.addWidget(self.closeButton)
-    self.filterlayout.addWidget(self.autoFilterCheckbox)
-    self.filterlayout.addWidget(self.filterText)
+        self.ctr_f_shortcut = QShortcut(QtGui.QKeySequence(QtCore.Qt.CTRL | QtCore.Qt.Key_F), self.parent, activated=self.ActionShowFilter)
 
-    self.ctr_f_shortcut = QShortcut(QtGui.QKeySequence(QtCore.Qt.CTRL | QtCore.Qt.Key_F), self.parent, activated=self.ActionShowFilter)
+        self.table = QtWidgets.QTableView()
+        self.table.sortByColumn(0, QtCore.Qt.AscendingOrder)
+        self.table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+        # self.table.setAlternatingRowColors(True)
+        self.layout.addWidget(self.table)
+        self.layout.addWidget(self.filterWidget)
+        self.parent.setLayout(self.layout)
 
-    self.table = QtWidgets.QTableView()
-    self.table.sortByColumn(0, QtCore.Qt.AscendingOrder)
-    self.table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
-    #self.table.setAlternatingRowColors(True)
-    self.layout.addWidget(self.table)
-    self.layout.addWidget(self.filterWidget)
-    self.parent.setLayout(self.layout)
+        self.model = ExportTableModel(self.GetExportData())
+        self.proxymodel = QtCore.QSortFilterProxyModel()
+        # Region: This is for the Auto Filter Toggle
+        self.isCodeFilterModel = QtCore.QSortFilterProxyModel()
+        self.isCodeFilterModel.setSourceModel(self.model)
+        self.isCodeFilterModel.setFilterKeyColumn(self.model.columns.index("Is Code"))
+        self.noRetnFilterModel = QtCore.QSortFilterProxyModel()
+        self.noRetnFilterModel.setSourceModel(self.isCodeFilterModel)
+        self.noRetnFilterModel.setFilterKeyColumn(self.model.columns.index("Starting Mnem"))
+        # End Region
+        self.proxymodel.setSourceModel(self.noRetnFilterModel)
+        self.proxymodel.setFilterKeyColumn(-1)
+        self.table.setModel(self.proxymodel)
+        self.table.doubleClicked.connect(self.ActionCellDoubleClicked)
+        self.table.activated.connect(self.ActionCellDoubleClicked)
+        self.table.setSortingEnabled(True)
 
-    self.model = ExportTableModel(self.GetExportData())
-    self.proxymodel = QtCore.QSortFilterProxyModel()
-    ## Region: This is for the Auto Filter Toggle
-    self.isCodeFilterModel = QtCore.QSortFilterProxyModel()
-    self.isCodeFilterModel.setSourceModel(self.model)
-    self.isCodeFilterModel.setFilterKeyColumn(self.model.columns.index("Is Code"))
-    self.noRetnFilterModel = QtCore.QSortFilterProxyModel()
-    self.noRetnFilterModel.setSourceModel(self.isCodeFilterModel)
-    self.noRetnFilterModel.setFilterKeyColumn(self.model.columns.index("Starting Mnem"))
-    ## End Region
-    self.proxymodel.setSourceModel(self.noRetnFilterModel)
-    self.proxymodel.setFilterKeyColumn(-1)
-    self.table.setModel(self.proxymodel)
-    self.table.doubleClicked.connect(self.ActionCellDoubleClicked)
-    self.table.activated.connect(self.ActionCellDoubleClicked)
-    self.table.setSortingEnabled(True)
+        self.table.setVisible(False)
+        self.table.resizeColumnsToContents()
+        self.table.horizontalHeader().setStretchLastSection(True)
+        self.table.setShowGrid(False)
+        self.table.setVisible(True)
 
-    self.table.setVisible(False)
-    self.table.resizeColumnsToContents()
-    self.table.horizontalHeader().setStretchLastSection(True)
-    self.table.setShowGrid(False)
-    self.table.setVisible(True)
+    def Show(self, caption, options=0):
+        super().Show(caption, options)
 
-  def Show(self, caption, options=0):
-    super().Show(caption, options)
-
-  def OnClose(self, form):
-    pass
-
+    def OnClose(self, form):
+        pass
